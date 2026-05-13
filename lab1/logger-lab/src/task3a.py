3f172bd45689f97ecd94e4cdb10f435a46f270fb
Chenkun Zhang <ch385414@dal.ca>
2026-05-13 14:01:46 -0300

    finished lab1

---
+++ lab1/logger-lab/src/task3a.py
@@ -0,0 +1,16 @@
+## import  logging
+# a = 5 
+# b = 0
+
+# try:
+#  c = a / b
+#except Exception as e:
+#  logging.error("Exception occurred", exc_info=True)
+## 
+import logging
+a = 5
+b = 0
+try:
+    c = a / b
+except Exception as e:
+    logging.exception("Exception occurred")
